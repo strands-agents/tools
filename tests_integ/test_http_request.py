b5656e2af51979f17b7f979a1136e29f8979663e
Dean Schmigelski <dbschmigelski+github@gmail.com>
2025-07-23 19:10:53 -0400

    fix: address security concern with binding to all interfaces in test_http_request

--- tests_integ/test_http_request.py
+++ tests_integ/test_http_request.py
@@ -129,7 +129,7 @@ class LocalHttpRequestHandler(BaseHTTPRequestHandler):
 def _find_free_port() -> int:
     """Find and return an available port."""
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind(("", 0))
+        s.bind(("127.0.0.1", 0))
         return s.getsockname()[1]
 
 
