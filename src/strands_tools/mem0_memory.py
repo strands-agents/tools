452ee98f34c94f3ae8d9d45b29aa34858b5c3af9
Andy Kwok <andy.kwok@improving.com>
2025-08-27 12:43:56 -0700

    Update format
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>

--- src/strands_tools/mem0_memory.py
+++ src/strands_tools/mem0_memory.py
@@ -225,14 +225,11 @@ class Mem0ServiceClient:
             An configuration dict with graph backend.
         """
         config = config or {}
-        config['graph_store'] = {
+        config["graph_store"] = {
             "provider": "neptune",
-            "config": {
-                "endpoint": f"neptune-graph://{os.environ.get('NEPTUNE_ANALYTICS_GRAPH_IDENTIFIER')}"
-            }
+            "config": {"endpoint": f"neptune-graph://{os.environ.get('NEPTUNE_ANALYTICS_GRAPH_IDENTIFIER')}"},
         }
         return config
-
 
     def _initialize_opensearch_client(self, config: Optional[Dict] = None) -> Mem0Memory:
         """Initialize a Mem0 client with OpenSearch backend.
