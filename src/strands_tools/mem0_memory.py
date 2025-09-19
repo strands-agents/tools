95bcbd130c0bc1261109fbbb2786b5cd9ddecd19
Andy Kwok <andy.kwok@improving.com>
2025-09-19 11:19:12 -0700

    Add config
    Signed-off-by: Andy Kwok <andy.kwok@improving.com>

--- src/strands_tools/mem0_memory.py
+++ src/strands_tools/mem0_memory.py
@@ -154,14 +154,14 @@ class Mem0ServiceClient:
             "provider": os.environ.get("MEM0_EMBEDDER_PROVIDER", "aws_bedrock"),
             "config": {"model": os.environ.get("MEM0_EMBEDDER_MODEL", "amazon.titan-embed-text-v2:0")},
         },
-        "llm": {
-            "provider": os.environ.get("MEM0_LLM_PROVIDER", "aws_bedrock"),
-            "config": {
-                "model": os.environ.get("MEM0_LLM_MODEL", "anthropic.claude-3-5-haiku-20241022-v1:0"),
-                "temperature": float(os.environ.get("MEM0_LLM_TEMPERATURE", 0.1)),
-                "max_tokens": int(os.environ.get("MEM0_LLM_MAX_TOKENS", 2000)),
-            },
-        },
+        # "llm": {
+        #     "provider": os.environ.get("MEM0_LLM_PROVIDER", "aws_bedrock"),
+        #     "config": {
+        #         "model": os.environ.get("MEM0_LLM_MODEL", "anthropic.claude-3-5-haiku-20241022-v1:0"),
+        #         "temperature": float(os.environ.get("MEM0_LLM_TEMPERATURE", 0.1)),
+        #         "max_tokens": int(os.environ.get("MEM0_LLM_MAX_TOKENS", 2000)),
+        #     },
+        # },
         "vector_store": {
             "provider": "opensearch",
             "config": {
@@ -206,7 +206,7 @@ class Mem0ServiceClient:
 
         if os.environ.get("NEPTUNE_ANALYTICS_GRAPH_IDENTIFIER"):
             logger.debug("Using Neptune Analytics graph backend (Mem0Memory with Neptune Analytics)")
-            config = self._configure_neptune_analytics_backend(config)
+            return self._configure_neptune_analytics_backend(config)
 
         if os.environ.get("OPENSEARCH_HOST"):
             logger.debug("Using OpenSearch backend (Mem0Memory with OpenSearch)")
@@ -225,11 +225,19 @@ class Mem0ServiceClient:
             An configuration dict with graph backend.
         """
         config = config or {}
+        config["vector_store"] = {
+            "provider": "neptune",
+            "config": {
+                "collection_name": os.environ.get("NEPTUNE_ANALYTICS_VECTOR_COLLECTION", "mem0"),
+                "endpoint": f"neptune-graph://{os.environ.get('NEPTUNE_ANALYTICS_GRAPH_IDENTIFIER')}"},
+        }
         config["graph_store"] = {
             "provider": "neptune",
             "config": {"endpoint": f"neptune-graph://{os.environ.get('NEPTUNE_ANALYTICS_GRAPH_IDENTIFIER')}"},
         }
-        return config
+        merged_config = self._merge_config(config)
+        merged_config = Mem0Memory.from_config(config_dict=merged_config)
+        return merged_config
 
     def _initialize_opensearch_client(self, config: Optional[Dict] = None) -> Mem0Memory:
         """Initialize a Mem0 client with OpenSearch backend.
